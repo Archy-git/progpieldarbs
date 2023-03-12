6838492c5935f15b296fcc0c9afb6eabe3bd4210
Archy-git <75359864+Archy-git@users.noreply.github.com>
2023-03-13 01:08:18 +0200

    Update registerPage.js

--- nobeigumadarbsiesniegsanai/frontend/src/views/registerPage.js
+++ nobeigumadarbsiesniegsanai/frontend/src/views/registerPage.js
@@ -15,10 +15,23 @@ function Register() {
   // Šis komponents definē veidlapas iesniegšanas apstrādātāja funkciju, kas pārbauda, vai parole atbilst noteiktām prasībām, pirms izsauc funkciju, lai reģistrētu jaunu lietotāju.
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,}/.test(password)) {
-      window.alert("Password must be at least 8 characters long, contain a digit, one capital letter and one special character.");
-    } else {
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumber = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>_]/.test(password);
+    console.log(hasUpperCase)
+    console.log(hasLowerCase)
+    console.log(hasNumber)
+    console.log(hasSpecialChar)
+    if (
+      hasUpperCase &&
+      hasLowerCase &&
+      hasNumber &&
+      hasSpecialChar
+    ) {
       registerUser(username, password, password2);
+    } else {
+      window.alert("Password must be at least 8 characters long, contain a digit, one capital letter and one special character.");
     }
   };
 
